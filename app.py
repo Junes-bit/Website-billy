12353ffd3886c463dc799dabd4d032b680085a19
Junes-bit <s.bensalah@gmx.de>
2026-05-23 09:13:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import os
 
 app = Flask(__name__)
+
+players = {}  # {name: score}
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/save_score", methods=["POST"])
+def save_score():
+
+    data = request.json
+    name = data["name"]
+    score = data["score"]
+
+    players[name] = max(players.get(name, 0), score)
+
+    return jsonify({"success": True})
+
+
+@app.route("/leaderboard")
+def leaderboard():
+
+    sorted_players = sorted(
+        players.items(),
+        key=lambda x: x[1],
+        reverse=True
+    )
+
+    return jsonify(sorted_players[:10])
+
+
 if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", 5000))
+
     app.run(host="0.0.0.0", port=port)

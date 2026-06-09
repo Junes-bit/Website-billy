273797c6ebd79f5ca04e7a594835451f69fed5b3
Junes-bit <s.bensalah@gmx.de>
2026-06-09 15:33:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -598,5 +598,5 @@ def spin_wheel():
 
 # ============= RUN =============
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 500))
     app.run(host="0.0.0.0", port=port)
